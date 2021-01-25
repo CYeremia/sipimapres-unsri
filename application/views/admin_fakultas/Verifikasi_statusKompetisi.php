72afa1ad89623c66d3f1ff2eeca3e0f4907cb32c
Christofer Yeremia <Cyeremia57@gmail.com>
2021-01-25 22:23:03 +0700

    fix link download file verifikasi-admin fakultas

--- application/views/admin_fakultas/Verifikasi_statusKompetisi.php
+++ application/views/admin_fakultas/Verifikasi_statusKompetisi.php
@@ -99,12 +99,12 @@
 
                                 <tr>
                                     <td style="width: 20%">Bukti Prestasi</td>
-                                    <td><a href="<?= base_url() ?>/admin_fakultas/downloadfileBuktiPrestasi/<?= $IDM->BuktiPrestasi ?>">Download <i class="fas fa-download"></i></a></td>
+                                    <td><a href="<?= base_url() ?>admin_fakultas/downloadfileBuktiPrestasi/<?= $IDM->BuktiPrestasi ?>">Download <i class="fas fa-download"></i></a></td>
                                 </tr>
 
                                 <tr>
                                     <td style="width: 20%">Dokumentasi Kegiatan</td>
-                                    <td><a href="<?= base_url() ?>/admin_fakultas/downloadfileDokumentasi/<?= $IDM->BuktiDokumentasi ?>">Download <i class="fas fa-download"></i></a></td>
+                                    <td><a href="<?= base_url() ?>admin_fakultas/downloadfileDokumentasi/<?= $IDM->BuktiDokumentasi ?>">Download <i class="fas fa-download"></i></a></td>
                                 </tr>
 
                                 <tr>
